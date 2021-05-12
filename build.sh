43dbba1973a6a812fea2c33eef68b950893dbd51
Ben Jackson <puremourning@gmail.com>
2021-05-12 11:43:55 +0100

    Make build work without signing again

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 bundle install
 
-xcodebuild clean build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO \
+xcodebuild clean build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO \
   | bundle exec xcpretty -r json-compilation-database -o compile_commands.json
